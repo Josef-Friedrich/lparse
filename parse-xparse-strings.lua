f71c128e101b988c96ed37b4e3cad8dfbea4589c
Josef Friedrich <josef@friedrich.rocks>
2023-01-28 12:57:32 +0100

    Play with lparse

--- parse-xparse-strings.lua
+++ parse-xparse-strings.lua
@@ -22,12 +22,13 @@ local patt = Pattern({
   'init',
   init = Variable('whitespace') ^ 0 *
     CaptureFolding(CaptureTable('') * Variable('list'), add_result),
-  list = (Variable('arg') * Variable('whitespace') ^ 0) ^ 1,
-  arg = Variable('m') + Variable('o') + Variable('s'),
+  list = (Variable('arg') * Variable('whitespace') ^ 1) ^ 1,
+  arg = Variable('m') + Variable('o') + Variable('s') + Variable('R'),
   m = Pattern('m') * CaptureConstant({ optional = false }),
   o = Pattern('o') * CaptureConstant({ optional = true }),
   s = Pattern('s') * CaptureConstant({ star = true }),
-
+  R = Pattern('R') * Range('az') * Range('az') *
+    CaptureConstant({ required = true }),
   whitespace = Set(' \t\n\r'),
 })
 
@@ -35,4 +36,4 @@ local function parse(s)
   return patt:match(s)
 end
 
-print(inspect(parse(' o s m m oo ')))
+print(inspect(parse(' o s m m o Rab')))
