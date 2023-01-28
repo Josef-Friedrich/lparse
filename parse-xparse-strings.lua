3fa68103cc0a98e394d4634a98a5541a782e50ed
Josef Friedrich <josef@friedrich.rocks>
2023-01-28 10:16:49 +0100

    Return a table

--- parse-xparse-strings.lua
+++ parse-xparse-strings.lua
@@ -1,4 +1,5 @@
--- local lpeg = require('lpeg')
+local inspect = require('inspect')
+local lpeg = require('lpeg')
 local Variable = lpeg.V
 local Pattern = lpeg.P
 local Set = lpeg.S
@@ -9,26 +10,29 @@ local CaptureTable = lpeg.Ct
 local CaptureConstant = lpeg.Cc
 local CaptureSimple = lpeg.C
 
-local function add(acc, newvalue)
-  print(newvalue)
+local function add_result(result, value)
+  if not result then
+    result = {}
+  end
+  table.insert(result, value)
+  return result
 end
 
 local patt = Pattern({
   'init',
-  init = Variable('whitespace') ^ 0 * CaptureFolding(CaptureTable('') * Variable('list'), add),
+  init = Variable('whitespace') ^ 0 *
+    CaptureFolding(CaptureTable('') * Variable('list'), add_result),
   list = (Variable('arg') * Variable('whitespace') ^ 0) ^ 1,
-  arg = Variable('marg') + Variable('oarg'),
-  marg = Pattern('m') * CaptureConstant('marg'),
-  oarg = Pattern('o') * CaptureConstant('oarg'),
+  arg = Variable('m') + Variable('o') + Variable('s'),
+  m = Pattern('m') * CaptureConstant({ optional = false }),
+  o = Pattern('o') * CaptureConstant({ optional = true }),
+  s = Pattern('s') * CaptureConstant({ star = true }),
+
   whitespace = Set(' \t\n\r'),
 })
 
 local function parse(s)
-  lpeg.match(patt, s)
+  return patt:match(s)
 end
 
-parse(' o m m  ')
-
--- + (Variable('whitespace') ^ 0) ^ -1)
-
--- CaptureFolding(add)
+print(inspect(parse(' o s m m oo ')))
